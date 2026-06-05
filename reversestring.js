01118bbd8cbad09f1179ae7fc93d201273e313a6
pujajathar <pujajathar91@.com>
2026-06-05 11:11:31 -0500

    feature_branch

--- reversestring.js
+++ reversestring.js
@@ -11,3 +11,18 @@ console.log(reverseString("nice"));
 console.log(reverseString("recursion"));
 console.log(reverseString("a"));
 console.log(reverseString(" "));
+
+//Reversing string using recursion
+function reverseString(str)
+{
+  if(str.length <= 1){
+    return str;
+}
+  return str[str.length - 1] + reverseString(str.slice(0, -1));
+}
+console.log(reverseString("Hello"));
+console.log(reverseString("nice"));
+console.log(reverseString("recursion"));
+console.log(reverseString("a"));
+console.log(reverseString(" "));
+
